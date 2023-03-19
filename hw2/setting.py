3596be204b27766c9bc9d6717c594953c9679c4f
lolainta <lolainta@gmail.com>
2023-03-19 10:05:16 +0800

    style: reformat sample code

--- hw2/setting.py
+++ hw2/setting.py
@@ -1,9 +1,13 @@
 def get_hosts():
     command = 'h1 h2 h3 h4 h5 h6 h7 h8'
-    return command 
+    return command
+
+
 def get_switches():
     command = 's1 s2 s3 s4 s5 s6 s7'
-    return command 
+    return command
+
+
 def get_ip():
     ip_dict = dict()
     ip_dict["h1"] = 'h1ip'
@@ -15,6 +19,8 @@ def get_ip():
     ip_dict["h7"] = 'h7ip'
     ip_dict["h8"] = 'h8ip'
     return ip_dict
+
+
 def get_mac():
     mac_dict = dict()
     mac_dict["h1"] = 'h1mac'
@@ -26,7 +32,8 @@ def get_mac():
     mac_dict["h7"] = 'h7mac'
     mac_dict["h8"] = 'h8mac'
     return mac_dict
-    
+
+
 def get_links():
     command = 'h1,s1 h2,s1 h3,s3 h4,s3 s1,s2 s2,s3 h5,s4 h6,s4 h7,s6 h8,s6 s4,s5 s5,s6 s2,s7 s5,s7'
     return command
