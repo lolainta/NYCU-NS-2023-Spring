1b30b44ff32c0f34772fdb84e856a1f071ad9de6
lolainta <lolainta@gmail.com>
2023-04-01 15:49:27 +0800

    feat: setting with simple coef

--- hw3/setting.py
+++ hw3/setting.py
@@ -2,13 +2,24 @@ import random
 
 
 class Setting():
-    def __init__(self, host_num=3, total_time=10000, packet_num=500, packet_size=5, max_colision_wait_time=20, p_resend=0.3, c=8, link_delay=1, seed=None) -> None:
+    def __init__(self,
+                 host_num: int = 3,
+                 total_time: int = 10000,
+                 packet_num: int = 500,
+                 packet_size: int = 5,
+                 max_colision_wait_time: None | int = None,
+                 p_resend: None | float = None,
+                 c: int = 8,
+                 link_delay: int = 1,
+                 seed: int | None = None
+                 ) -> None:
         self.host_num = host_num
         self.total_time = total_time
         self.packet_num = packet_num
         self.packet_time = packet_size + 2*link_delay
-        self.max_colision_wait_time = 20 if max_colision_wait_time is None else max_colision_wait_time
-        self.p_resend = 0.5 if p_resend is None else p_resend
+        self.max_colision_wait_time = self.packet_time * \
+            c if max_colision_wait_time is None else max_colision_wait_time
+        self.p_resend = 1/c if p_resend is None else p_resend
         self.link_delay = link_delay
         if seed is None:
             self.seed = random.randint(1, 10000)
