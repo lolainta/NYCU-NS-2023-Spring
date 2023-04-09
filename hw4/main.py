64ad97910814d87e1591f1f6a8933b9b0d7bfe02
lolainta <lolainta@gmail.com>
2023-04-09 16:18:39 +0800

    fix: fix sample mini data for rip

--- hw4/main.py
+++ hw4/main.py
@@ -111,16 +111,11 @@ def main():
             (5, 2), (5, 4),
             (0, 1), (0, 2), (0, 3),
             (1, 0), (1, 2), (1, 3),
-            (2, 0), (2, 1), (2, 3), (2, 4), (2, 5),
-            (3, 0),
-            (2, 3), (2, 4), (2, 5),
-            (3, 0),
-            (4, 3), (4, 5),
-            (5, 2), (5, 4)
+            (2, 0), (2, 1), (2, 3), (2, 4), (2, 5)
         ]
     )
     assert mini_ospf == run_ospf(mini_data[0]), 'OSPF Failed on mini_data'
-    assert mini_rip[0] == run_rip(mini_data[0])[0], 'RIP Failed on mini_data'
+    assert mini_rip == run_rip(mini_data[0]), 'RIP Failed on mini_data'
     assert len(testdata) == len(ans_ospf)
     assert len(testdata) == len(ans_rip)
     for data, ospf, rip in zip(testdata, ans_ospf, ans_rip):
