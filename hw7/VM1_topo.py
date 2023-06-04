640fcb80057fd2fa2256f72faecf443385b7e18f
LolaInTa Chi <lolainta@gmail.com>
2023-06-04 14:23:35 +0800

    fix: miniet ip/24

--- hw7/VM1_topo.py
+++ hw7/VM1_topo.py
@@ -4,10 +4,10 @@ from mininet.topo import Topo
 class NSCAP_HW7(Topo):
     def build(self):
         # Add hosts and switches
-        h1 = self.addHost("h1", ip="10.0.0.1", mac="00:00:00:00:00:01")
-        h2 = self.addHost("h2", ip="10.0.0.2", mac="00:00:00:00:00:02")
-        h3 = self.addHost("h3", ip="10.0.0.3", mac="00:00:00:00:00:03")
-        h4 = self.addHost("h4", ip="10.0.0.4", mac="00:00:00:00:00:04")
+        h1 = self.addHost("h1", ip="10.0.0.1/24", mac="00:00:00:00:00:01")
+        h2 = self.addHost("h2", ip="10.0.0.2/24", mac="00:00:00:00:00:02")
+        h3 = self.addHost("h3", ip="10.0.0.3/24", mac="00:00:00:00:00:03")
+        h4 = self.addHost("h4", ip="10.0.0.4/24", mac="00:00:00:00:00:04")
         sw = self.addSwitch("s1")
 
         # Add links
